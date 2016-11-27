0b47e19b2a1c2d24952d0c2c5317560b12e86b32
liubingchuan8 <liubingchuan8@gmail.com>
2016-11-27 23:22:20 +0800

    左侧查词，添加status2，按照published和unpublished进行判断

--- src/main/java/controller/WordResource.java
+++ src/main/java/controller/WordResource.java
@@ -337,6 +337,22 @@ public class WordResource {
 			return Response.status(200).entity("").type("text/plain").build();
 		}
 		List<Word> list = this.wordService.findWordByName(wordName, logon);
+		boolean published = false;
+		for(Word word: list) {
+			if("published".equals(word.getStatus())) {
+				published = true;
+				break;
+			}
+		}
+		if (published == true) {
+			for(Word word: list) {
+				word.setStatus2("published");
+			}
+		}else {
+			for(Word word: list) {
+				word.setStatus2("unpublished");
+			}
+		}
 
 		LOGGER.info(
 				"return the words detail info------" + new SimpleDateFormat("yyyy MM dd HH mm ss").format(new Date()));
